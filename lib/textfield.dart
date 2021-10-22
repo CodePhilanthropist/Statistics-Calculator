4f25b2fa2b3b290c3b66ec7ec0c818bf895d08a6
CodePhilanthropist <barrigaete@gmail.com>
2021-10-22 10:47:13 +0800

    add controller

--- lib/textfield.dart
+++ lib/textfield.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class TextFieldCustom extends StatefulWidget {
-  const TextFieldCustom({ Key? key }) : super(key: key);
+  var _controller = TextEditingController();
+  TextField({controller}) {
+    this._controller = controller;
+  }
 
   @override
   _TextFieldCustomState createState() => _TextFieldCustomState();
@@ -10,8 +13,29 @@ class TextFieldCustom extends StatefulWidget {
 class _TextFieldCustomState extends State<TextFieldCustom> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: TextField(
+        controller: _controller,
+        onChanged: (text) {
+          setState(() {});
+        },
+        decoration: InputDecoration(
+          hintText: "Enter data",
+          labelText: "Number ${_counter + 1}",
+          labelStyle: TextStyle(fontSize: 24),
+          border: OutlineInputBorder(),
+          suffixIcon: _controller.text.length > 0
+              ? IconButton(
+                  onPressed: () {
+                    _controller.clear();
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.cancel, color: Colors.grey))
+              : null,
+        ),
+        keyboardType: TextInputType.number,
+      ),
     );
   }
 }
