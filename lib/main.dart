9f0010041d7a8755a052aca5e5f0994081d352f5
CodePhilanthropist <barrigaete@gmail.com>
2021-10-22 17:20:14 +0800

    add counter

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   List<int> nums = [];
-  var _controller = TextEditingController();
+  
 
   void _incrementCounter() {
     setState(() {
