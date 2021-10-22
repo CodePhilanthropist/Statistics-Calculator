29247432d491fc62376472290911b81655c7fe54
CodePhilanthropist <barrigaete@gmail.com>
2021-10-22 10:43:33 +0800

    add a new TextField

---
+++ lib/textfield.dart
@@ -0,0 +1,15 @@
+class TextFieldCustom extends StatefulWidget {
+  const TextFieldCustom({ Key? key }) : super(key: key);
+
+  @override
+  _TextFieldCustomState createState() => _TextFieldCustomState();
+}
+
+class _TextFieldCustomState extends State<TextFieldCustom> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
